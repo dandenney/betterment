f1a779953fff16ca35c03da81d9de22307afaeae
Dan Denney <dd.subscriptions@gmail.com>
2017-03-11 20:02:11 -0500

    feat: Add a form for creating a resource
    Right now, this just allows for adding a title. I want to get that
    outputting into the page and get resource into state before moving on.

--- src/components/CreateResourceForm.js
+++ src/components/CreateResourceForm.js
@@ -2,9 +2,23 @@
 import React, { Component } from 'react';
 
 class CreateResourceForm extends Component {
+
+  createResource(e) {
+    e.preventDefault();
+
+    const resource = {
+      title: this.title.value
+    }
+
+    this.resourceForm.reset();
+  }
+
   render() {
     return (
-      <p>CreateResourceForm</p>
+      <form className="resource-edit" onSubmit={ this.createResource.bind(this) } ref={ (input) => this.resourceForm = input }>
+        <input ref={ (input) => this.title = input } placeholder="Resource Name" type="text" />
+        <button type="submit">Add Resource</button>
+      </form>
     );
   }
 }
