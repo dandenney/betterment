cff147042e7691c8ddef4d2dc65513e66ed6872a
Dan Denney <dd.subscriptions@gmail.com>
2017-03-11 19:22:42 -0500

    feat: Add resource component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 
 // App
 import Header from './components/Header';
+import Resource from './components/Resource';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Resource />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Resource.js
@@ -0,0 +1,12 @@
+// Global
+import React, { Component } from 'react';
+
+class Resource extends Component {
+  render() {
+    return (
+      <p>Resource</p>
+    );
+  }
+}
+
+export default Resource;
