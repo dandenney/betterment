43c9a11bea73f5da1ed24071827c7b2bc2b399de
Dan Denney <dd.subscriptions@gmail.com>
2017-03-22 21:07:38 -0400

    feat: Display creator's name and avatar

--- src/components/Resource.js
+++ src/components/Resource.js
@@ -1,15 +1,17 @@
 // Global
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Resource extends Component {
   render() {
-    const { details } = this.props;
+    const {details} = this.props;
 
     return (
       <article>
-        <h3>{ details.title }</h3>
-        <p>{ details.description }</p>
-        <o><a href={ details.url }>Link</a></o>
+        <h3>{details.title}</h3>
+        <p>{details.description}</p>
+        <p><a href={details.url}>Link</a></p>
+        <p>{details.name}</p>
+        <p><img alt={details.name} src={details.avatar} width="24" /></p>
       </article>
     );
   }
